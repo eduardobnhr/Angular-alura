f6879fceb3f1345f031d40bfb9dd9e7bcb351738
eduardo <eduardobnhr@gmail.com>
2023-09-25 13:37:09 -0400

    add component

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent  implements OnInit{
+
+  pensamento = {
+    conteudo: 'i love angular',
+    autoria: 'edu',
+    modelo: 'modelo3'
+  }
+
+  constructor(){}
+
+  ngOnInit(): void{
+
+  }
+
+}
