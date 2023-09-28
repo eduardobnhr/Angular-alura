b2616b65999d38effd069a04c2e5b9e97992dd58
eduardo <eduardobnhr@gmail.com>
2023-09-28 13:03:02 -0400

    Communication between components

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
   styleUrls: ['./listar-pensamento.component.css']
 })
-export class ListarPensamentoComponent {
+
+export class ListarPensamentoComponent implements OnInit{
+  listaPensamentos = [{
+    conteudo: 'Passo informações para componente filho',
+    autoria: 'componente pai',
+    modelo: 'modelo3'
+  },
+  {
+    conteudo: 'Minha propriedade é decorada com Input()',
+    autoria: 'componente filho',
+    modelo: 'modelo2'
+  }
+];
+
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 
 }
