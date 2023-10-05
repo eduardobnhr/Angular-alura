2fb31f9d1d52654353f1cfa93146d3439d0990c2
eduardobnhr <eduardobnhr@gmail.com>
2023-10-05 17:39:28 -0300

    alteracao no ngclass

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -22,7 +22,7 @@ export class PensamentoComponent  implements OnInit{
     if(this.pensamento.conteudo.length >= 256){
       return 'pensamento-g'
     }
-    return 'pensamento-g'
+    return 'pensamento-p'
   }
 
 }
